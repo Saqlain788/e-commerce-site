b146c3a351c63d175c7d0c4a939281df299efb58
Muhammad Saqlain <saqlain788@gmail.com>
2024-02-20 15:12:34 +0500

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,33 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-  import {MyNavbar} from "@/components/ui/navbar";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { MyNavbar } from "@/components/ui/navbar";
 import Footer from "@/components/ui/footer";
- 
- 
-const inter = Inter({ subsets: ['latin'] })
+
+const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
-    title: 'A ZenithMarket ',
-    description: 'Discover a world of quality products at ZenithMarket, your go-to ecommerce platform for an unparalleled shopping experience.',
-}
+  title: "Dine Market ",
+  description: "Discover a world of quality products at Dine Market.",
+};
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body >
-      <div className={'flex flex-col min-h-screen p-[1rem'}  style={{maxWidth:"1550px",margin:'auto'}}>
-         
-          <MyNavbar/>
-          <div className={'flex-grow my-8 lg:px-8 md:px-4 px-4'}>{children}</div>
-          <Footer/>
-      </div>
+      <body>
+        <div
+          className={"flex flex-col min-h-screen p-[1rem"}
+          style={{ maxWidth: "1550px", margin: "auto" }}
+        >
+          <MyNavbar />
+          <div className={"flex-grow my-8 lg:px-8 md:px-4 px-4"}>
+            {children}
+          </div>
+          <Footer />
+        </div>
       </body>
     </html>
-  )
+  );
 }
